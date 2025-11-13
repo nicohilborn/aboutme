0d677ad136aa39c9ff5225c4c2cf393737e77bdc
nicohilborn <nicohilborn@gmail.com>
2025-11-13 16:04:16 -0600

    changes

--- function.js
+++ function.js
@@ -2,3 +2,9 @@ function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+ function changeColor() {
+      const text = document.getElementById("colorText");
+      const colors = ["#ff4d4d", "#4dff88", "#4db8ff", "#ffdb4d", "#d24dff"];
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      text.style.color = randomColor;
+    }
