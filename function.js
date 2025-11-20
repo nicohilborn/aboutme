362079faf5527de567a2267fae551698009eb67a
nicohilborn <nicohilborn@gmail.com>
2025-11-20 15:29:42 -0600

    correct changes

---
+++ function.js
@@ -0,0 +1,10 @@
+function myFunction() {
+   let element = document.body;
+   element.classList.toggle("dark-mode");
+}
+ function changeColor() {
+      const text = document.getElementById("colorText");
+      const colors = ["#ff4d4d", "#4dff88", "#4db8ff", "#ffdb4d", "#d24dff"];
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      text.style.color = randomColor;
+    }
